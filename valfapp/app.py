2fb1854f6f81a0ce5b8c224aba9cc555cf0d3a1f
kbbudak <kemalbasarbudak@gmail.com>
2023-09-28 14:44:41 +0300

    syns to master

--- valfapp/app.py
+++ valfapp/app.py
@@ -18,9 +18,7 @@ app = dash.Dash(
     meta_tags=[{'name': 'viewport','content': 'width=device-width, initial-scale=1.0, maximum-scale=1.2, minimum-scale=0.5,'}],
     external_scripts=["https://cdnjs.cloudflare.com/ajax/libs/dragula/3.7.2/dragula.min.js"],
     external_stylesheets=[dbc.themes.PULSE],
-    suppress_callback_exceptions=True,
-    meta_tags=[{'name': 'viewport',
-                            'content': 'width=device-width, initial-scale=1.0, maximum-scale=1.2, minimum-scale=0.5,'}])
+    suppress_callback_exceptions=True)
 
 app.css.append_css({
     "external_url": (
