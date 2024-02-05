e945937396f8b6a41d96feb60e4398464c371a05
kbbudak <kemalbasarbudak@gmail.com>
2024-02-05 12:46:32 +0300

    prd energy report download button fixed. v3.

--- valfapp/pages/prd_energy.py
+++ valfapp/pages/prd_energy.py
@@ -36,17 +36,8 @@ layout = [
             dbc.Col(
                 html.Div(
                     [
-                        dcc.Dropdown(
-                            id='machine-type-dropdown2',
-                            options=[
-                                {'label': k, 'value': k}
-                                for k in valftoreeg.keys()
-                            ],
-                            value=list(valftoreeg.keys())[0],
-                            style={
-                                'color': 'white',
-                                'width': 220,
-                            },
+                        dcc.Input(
+                            id='text-input', type='text', value='Enter material...'
                         )], style={"margin-top": 18}), width=1),
             dbc.Col(
                 html.Div(
@@ -170,26 +161,110 @@ def set_machine_options(selected_machine_type):
 @app.callback(
     Output("data_table2", "data"),
     Output("data_table2", "columns"),
+    Output('generated_data2', 'data'),
     [State('date-picker2', 'start_date'),
      State('date-picker2', 'end_date'),
-     State('machine-type-dropdown2', 'value'),
+     State('text-input', 'value'),
      State('machine-dropdown2', 'value'),
      State('date-dropdown2', 'value'),
      Input('search2', 'n_clicks')]
 )
-def cache_to_result(s_date, f_date, costcenter, m_point, date_interval, button):
+def cache_to_result(s_date, f_date, input_material, m_point, date_interval, button):
     if button <= 0:
         raise PreventUpdate
-    df = ag.run_query(f"SELECT * FROM VLFPRDENERGYVİEW")
+    print("***")
+    print(input_material)
+    if input_material == 'Enter material...' or input_material == '':
+        df = ag.run_query(f"SELECT * FROM VLFPRDENERGYVİEW")
+    else:
+        df = ag.run_query(f"SELECT * FROM VLFPRDENERGYVİEW WHERE MATERIAL = '{input_material}'")
+
     df["TOTWEIGHT"] = df["TOTWEIGHT"].fillna(0)
     df["KWHPERTON"] = df.apply(lambda x: (x["TOTKWH"] / x["TOTWEIGHT"]) if x["TOTWEIGHT"] != 0 else 1.111, axis=1)
     df['KWHPERTON'] = df['KWHPERTON'].apply(lambda x: Decimal(x).quantize(Decimal('0.000')))
-
     pivot_table = df.pivot_table(index="MATERIAL", columns='COSTCENTER', values='KWHPERTON', aggfunc='first')
     pivot_table.reset_index(inplace=True)
+
+    pivot_table.columns = [col.replace(' ', '') for col in pivot_table.columns]
+
+    if input_material == 'Enter material...' or input_material == '':
+        print("here")
+
+        pivot_table = pivot_table[['MATERIAL','PRESHANE1','PRESHANE2','PRESHAN2','ISIL','ISOFINIS','ISOFINI2','KURUTMA','KURUTMA2','KURUTMA3','MENEVIS','TASLAMA','TASLAMA2','DOGRULTMA','PLKYZYIS','YIKAMA','CNC','CNCTORN2','CNCTORN3','CNCTORNA']]
+        columns_to_sum = ['PRESHANE1', 'PRESHANE2', 'PRESHAN2', 'ISIL', 'ISOFINIS', 'ISOFINI2', 'KURUTMA', 'KURUTMA2',
+                          'KURUTMA3', 'MENEVIS', 'TASLAMA', 'TASLAMA2', 'DOGRULTMA', 'PLKYZYIS', 'YIKAMA', 'CNC',
+                          'CNCTORN2', 'CNCTORN3', 'CNCTORNA']
+
+    else:
+        pivot_table["PAKET"] = 0.000
+        df_columns = ag.run_query(query=r"EXEC VLFPRDENERGYROTACHECK @MATERIAL=?", params=[input_material])
+        print("*********")
+        print(pivot_table.columns)
+        print("*********")
+        columns_to_convert = [col for col in pivot_table.columns if col not in ['MATERIAL', 'TOTAL']]
+        pivot_table[columns_to_convert] = pivot_table[columns_to_convert].apply(pd.to_numeric, errors='coerce')
+        for col in list(df_columns["COSTCENTER"].unique()):
+            if col not in pivot_table.columns:
+                pivot_table[col] = 0.000
+            else:
+                pivot_table[col] = pivot_table[col].astype(float)
+        print(df_columns["COSTCENTER"].unique())
+        print(pivot_table[df_columns["COSTCENTER"].unique()].sum(axis=1, skipna=True))
+        pivot_table['TOTAL'] = pivot_table[[x for x in list(pivot_table.columns) if x not in ['MATERIAL','TOTAL']]].sum(axis=1, skipna=True)
+
+    print("******")
+    print(pivot_table.dtypes)
+    print([x for x in list(pivot_table.columns) if x != 'MATERIAL'])
+    pivot_table_store = pivot_table.to_json(date_format='iso', orient='split')
     columns = [{"name": i, "id": i} for i in pivot_table.columns]
-    print("********************************")
-    return pivot_table.to_dict("records"), columns
+    print("***")
+    print(columns)
+    return pivot_table.to_dict("records"), columns,pivot_table_store
+
+# Assuming pivot_table is your DataFrame with the specified columns
+# Listing the columns you want to sum
+
+# Calculate the sum of the specified columns for each row
+
+# If you want to treat rows with all NaN values in the specified columns as 0 instead of NaN in the total
+# you can fill NaN values in the 'TOTAL' column with 0
+# pivot_table['TOTAL'] = pivot_table['TOTAL'].fillna(0)
+
+
+'''
+    def query_result(column_name, material):
+        # Placeholder for your actual query logic
+        # This function now simulates a database query
+        df1 = ag.run_query(
+            f"SELECT B.ANAMAMUL, A.COSTCENTER FROM VLFALTPARCA B INNER JOIN IASROUOPR A ON A.MATERIAL = B.MATERIAL WHERE B.ANAMAMUL = '{material}' AND A.COSTCENTER = '{column_name}'")
+        df2 = ag.run_query(
+            f"SELECT DISTINCT COSTCENTER FROM IASROUOPR WHERE MATERIAL = '{material}' AND COSTCENTER = '{column_name}'")
+        if df1.empty and df2.empty:
+            return 'x'
+        else:
+            return '0'
+
+    # Check for NaN and apply query_result function only if necessary
+    def fill_if_nan(column_name, material_column):
+        # Apply only to NaN values
+        def apply_query(value):
+            if pd.isna(value):
+                return query_result(column_name, material_column)
+            else:
+                return value
+
+        return apply_query
+
+    # Apply the optimized function
+    for column in pivot_table.columns:
+        if column != 'MATERIAL':  # Skipping the 'Material' column itself
+            material_column = pivot_table['MATERIAL']
+            # Create a mask for NaN values in the current column
+            nan_mask = pivot_table[column].isna()
+            # Apply query_result only for NaN values
+            pivot_table.loc[nan_mask, column] = pivot_table.loc[nan_mask].apply(
+                lambda row: fill_if_nan(column, row['MATERIAL'])(row[column]), axis=1)
+'''
 
 
 
@@ -199,8 +274,7 @@ def cache_to_result(s_date, f_date, costcenter, m_point, date_interval, button):
     prevent_initial_call=True
 )
 def generate_excel(n_clicks,):
-    print("here")
-    generated_data = ag.run_query(r"SELECT * FROM VLFPRDENERGYUNSTRUCTERED")
+    generated_data = ag.run_query(r"SELECT * FROM VLFPRDENERGY")
     if n_clicks < 1:
         raise PreventUpdate
 
@@ -208,13 +282,13 @@ def generate_excel(n_clicks,):
 
 @app.callback(
     Output("download-energy3", "data"),
+    State(component_id='generated_data2', component_property='data'),
     Input("download3", "n_clicks"),
 )
-def generate_excel2(n_clicks,):
-    print("here")
+def generate_excel2(generated_data,n_clicks,):
 
-    generated_data = ag.run_query(r"SELECT * FROM VLFPRDENERGYVİEW")
     if n_clicks < 1:
         raise PreventUpdate
+    generated_data = pd.read_json(generated_data, orient='split')
 
     return dcc.send_data_frame(generated_data.to_excel, "energydata.xlsx", index=False)
