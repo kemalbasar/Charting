34b612d13a4094c6b4ce39d6ffdaa580e26bde69
kbbudak <kemalbasarbudak@gmail.com>
2024-03-03 21:49:36 +0300

    workcenter page, layout logic rebuilded. login page omitted.

--- reengen.py
+++ reengen.py
@@ -1,49 +1,49 @@
-import requests
-import json
-from config import reengen_company,reengen_password,reengen_username,valftoreeg
-from run.agent import ag
-
-
-# API Endpoint
-url = "https://api.reengen.com/api/do/"
-
-# Authentication payload
-auth_payload = {
-    "$": "Authenticate",
-    "properties": {
-        "tenant": reengen_company,  # Replace with your tenant name
-        "user": reengen_username ,      # Replace with your username
-        "password": reengen_password  # Replace with your password
-    }
-}
-
-headers = {
-    'Content-Type': 'application/json'
-}
-
-# Make the POST request for auth
-response = requests.post(url, data=json.dumps(auth_payload), headers=headers)
-
-# Check if the request was successful
-if response.status_code == 200:
-    response_data = response.json()
-    if response_data["succeeded"]:
-        connection_id = response_data["properties"]["connectionId"]
-        print(f"Connection ID: {connection_id}")
-    else:
-        print(f"Authentication failed: {response_data['message']}")
-else:
-    print(f"HTTP Error: {response.status_code}")
-
-
-
-#get point hierarchy
-
+# import requests
+# import json
+# from config import reengen_company,reengen_password,reengen_username,valftoreeg
+# from run.agent import ag
+#
+#
+# # API Endpoint
+# url = "https://api.reengen.com/api/do/"
+#
+# # Authentication payload
+# auth_payload = {
+#     "$": "Authenticate",
+#     "properties": {
+#         "tenant": reengen_company,  # Replace with your tenant name
+#         "user": reengen_username ,      # Replace with your username
+#         "password": reengen_password  # Replace with your password
+#     }
+# }
+#
+# headers = {
+#     'Content-Type': 'application/json'
+# }
+#
+# # Make the POST request for auth
+# response = requests.post(url, data=json.dumps(auth_payload), headers=headers)
+#
+# # Check if the request was successful
+# if response.status_code == 200:
+#     response_data = response.json()
+#     if response_data["succeeded"]:
+#         connection_id = response_data["properties"]["connectionId"]
+#         print(f"Connection ID: {connection_id}")
+#     else:
+#         print(f"Authentication failed: {response_data['message']}")
+# else:
+#     print(f"HTTP Error: {response.status_code}")
+#
+#
+#
+# get point hierarchy
+#
 # ********************************************# ********************************************# ********************************************
 # ********************************************# ********************************************# ********************************************
-
+#
 # Headers
-
+#
 # headers = {
 #     'Content-Type': 'application/json',
 #     'XConnectionId': connection_id,       # Replace with your connection id
@@ -57,7 +57,7 @@ else:
 #
 # # Make the POST request
 # response = requests.post(url, data=json.dumps(payload), headers=headers)
-#
+# 0
 # # Check if the request was successful
 # if response.status_code == 200:
 #     response_data = response.json()
@@ -67,13 +67,13 @@ else:
 # get_hierarchy = { "$": "GetPointHierarchy"}
 #
 # response = requests.post(url, data=json.dumps(auth_payload), headers=headers)
-
-
 #
-
+#
+#
+#
 # ********************************************# ********************************************# ********************************************
 # ********************************************# ********************************************# ********************************************
-
+#
 # s_date = '20231002'
 # f_date = '20231005'
 # with open(f"F:\pycarhm projects\Charting\queries\mest_test.sql", 'r') as file:
@@ -81,46 +81,46 @@ else:
 # s_date = s_date.replace('-', '', 2)
 # f_date = f_date.replace('-', '', 2)
 # filedata = filedata.replace("xxxx-yy-zz", s_date)
-# filedata = filedata.replace("aaaa-bb-cc", f_date)
+# filedata = filedata.replace("aaaa-bb-cc", f_date)0
 # print(filedata)
 # df_works = ag.run_query(filedata)
 # df_works["CONSUMPTION"] = 0.00
-
+#
 # for i in range(len(df_works)):
 #     workcenter = df_works.iloc[i]["WORKCENTER"]
 #     code_wc = valftoreeg[workcenter]
-
-
-payload = {
-    "$": "GetData",
-    "properties": {
-        "series": [
-            {
-                "definition": "activeEnergy",
-                "variant": "import",
-                "type": "actual",
-                "xFunction": "sum",
-                "unit": "kWh",
-                "decimalPoints": 3
-            },
-        ],
-        "point": ["valfsan_otecsantrifuj"],
-        "start": "2023-10-12T17:20:39.000",
-        "end": "2023-10-12T18:40:58.000",
-        "break": {
-            "type": "point"
-        },
-        "resolution": "fifteenmin"
-    }
-}
-
-
-# Make the POST request
-response = requests.post(url, data=json.dumps(payload), headers=headers)
-
-# Check if the request was successful
-if response.status_code == 200:
-    response_data = response.json()
-    print(json.dumps(response_data, indent=4))  # Pretty print the JSON response
-else:
-    print(f"HTTP Error: {response.status_code}")
+#
+#
+# payload = {
+#     "$": "GetData",
+#     "properties": {
+#         "series": [
+#             {
+#                 "definition": "activeEnergy",
+#                 "variant": "import",
+#                 "type": "actual",
+#                 "xFunction": "sum",
+#                 "unit": "kWh",
+#                 "decimalPoints": 3
+#             },
+#         ],
+#         "point": ["valfsan_otecsantrifuj"],
+#         "start": "2023-10-12T17:20:39.000",
+#         "end": "2023-10-12T18:40:58.000",
+#         "break": {
+#             "type": "point"
+#         },
+#         "resolution": "fifteenmin"
+#     }
+# }
+#
+#
+# # Make the POST request
+# response = requests.post(url, data=json.dumps(payload), headers=headers)
+#
+# # Check if the request was successful
+# if response.status_code == 200:
+#     response_data = response.json()
+#     print(json.dumps(response_data, indent=4))  # Pretty print the JSON response
+# else:
+#     print(f"HTTP Error: {response.status_code}")
