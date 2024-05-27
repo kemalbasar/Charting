40758dec0ec7d76103f1c0a34ddc82e763c642ac
fozturk <fozturk@valfsan.com.tr>
2024-05-27 09:19:15 +0300

    canlı ekran kmr-ayk beta

--- valfapp/index.py
+++ valfapp/index.py
@@ -9,7 +9,7 @@ from valfapp.app import app
 from pages import value, prod_eff, workcenters,liveprd,dragtester
 from valfapp.layouts import layout_12_loginpage_v2
 from valfapp.pages import livecnc, livepres, energy, cnctotv, taslamatv, montajtv, yuzeyislemtv, kameraayıklama, \
-    tvmonitor, uretimrapor, kapasite, prd_energy, cnc1tv, cnc2tv, camayikuretim, adr_CNC, adr_CNCTORNA, \
+    tvmonitor, uretimrapor, kapasite, prd_energy, cnc1tv, cnc2tv, camayikuretim, adr_CNC, adr_CNCTORNA, livekamera, \
     adr_PRES1, adr_PRES2, adr_MONTAJ, gvt_page, adr_CNC_week, adr_PRES1_week, adr_PRES2_week, adr_CNCTORNA_week, \
     adr_MONTAJ_week, adr_CNC_month, adr_CNCTORNA_month, adr_MONTAJ_month, adr_PRES1_month, adr_PRES2_month, \
     adr_CNCreal, adr_CNCTORNAreal, \
@@ -147,6 +147,8 @@ def display_page(pathname,login_status_data):
         return gvt_page.layout
     elif pathname == '/camayikuretim':
         return camayikuretim.layout
+    elif pathname == '/livekamera':
+        return livekamera.layout
     elif pathname == '/costing':
         return costing.layout
 
@@ -244,6 +246,7 @@ app.validation_layout = html.Div(
         yuzeyislemtv.layout,
         kameraayıklama.layout,
         camayikuretim.layout,
+        livekamera.layout,
         tvmonitor.layout,
         uretimrapor.layout,
         kapasite.layout,
